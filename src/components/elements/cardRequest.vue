35dd48f4a1fff5e1ea027fa7d996394c96c57ea6
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-01-12 13:01:57 +0100

    draft first section main

---
+++ src/components/elements/cardRequest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>First Lesson Free!</h2>
+    <p>When you make a block booking with us your first lesson is included free.</p>
+    <input type="text" placeholder="Your Name*">
+    <input type="email" placeholder="Email*">
+    <input type="tel" placeholder="Thelephone">
+    <input type="text" placeholder="Location">
+    <div class="btnGreen">request a call back</div>
+
+  </div>
+</template>
+
+<script>
+export default { 
+    name: 'cardRequest'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
