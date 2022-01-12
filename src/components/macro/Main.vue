712571319cd26c49be24a43cb99b1587e403f8d3
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-01-12 13:02:23 +0100

    darft first section main

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,12 +1,35 @@
 <template>
   <main>
-
+    <GetBehindTheWheel/>
+    <StartLearningToDrive/>
+    <Courses/>
+    <Istructors/>
+    <Testimonials/>
+    <LatestNews/>
+    <Newsletter/>
   </main>
 </template>
 
 <script>
+import GetBehindTheWheel from '../section/GetBehindTheWheel.vue';
+import StartLearningToDrive from'../section/StartLearningToDrive.vue';
+import Courses from'../section/Courses.vue';
+import Istructors from'../section/Istructors.vue';
+import Testimonials from'../section/Testimonials.vue';
+import LatestNews from'../section/LatestNews.vue';
+import Newsletter from'../section/Newsletter.vue';
+
 export default {
     name: 'Main',
+    components:{
+      GetBehindTheWheel,
+      StartLearningToDrive,
+      Courses,
+      Istructors,
+      Testimonials,
+      LatestNews,
+      Newsletter,
+    }
 
 }
 </script>
---
+++ src/components/section/GetBehindTheWheel.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="container d-flex justify-content-between align-items-center">
+    <div class="left">
+        <h2>Get Behind The Wheel</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper. Fusce et ultrices libero, at suscipit lacus. Sed vitae fringilla nibh. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper. Fusce et ultrices libero, at suscipit lacus. Sed vitae fringilla nibh.</p>
+    </div>
+    <div class="rigth">
+        <cardRequest/>
+    </div>
+</div>
+</template>
+
+<script>
+import cardRequest from '../elements/cardRequest.vue';
+export default {
+name: 'GetBehindTheWheel',
+
+components:{
+    cardRequest,
+}
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
